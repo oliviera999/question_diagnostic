265d82e663fc9178f1f96d85990d07dce644723b
oliviera999 <oliv.arn.lau@gmail.com>
2025-12-13 17:11:20 +0100

    Increment version to v1.11.19

--- version.php
+++ version.php
@@ -9,8 +9,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_question_diagnostic';
-$plugin->version = 2025101518;  // YYYYMMDDXX format (v1.11.18 - FIX: Added missing class files)
+$plugin->version = 2025121300;  // YYYYMMDDXX format (v1.11.19 - Version increment)
 $plugin->requires = 2022041900; // Moodle 4.0+ (architecture question_bank_entries requise)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.11.18';
+$plugin->release = 'v1.11.19';
 
