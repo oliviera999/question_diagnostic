a7142ac3e064cd8c593e8f830a7e9005a2ac9380
oliviera999 <oliv.arn.lau@gmail.com>
2025-12-13 16:28:06 +0100

    Bump version to v1.11.18 (Added missing classes)

--- version.php
+++ version.php
@@ -9,8 +9,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_question_diagnostic';
-$plugin->version = 2025101517;  // YYYYMMDDXX format (v1.11.17 - AUDIT FIX: Standardized question moving API)
+$plugin->version = 2025101518;  // YYYYMMDDXX format (v1.11.18 - FIX: Added missing class files)
 $plugin->requires = 2022041900; // Moodle 4.0+ (architecture question_bank_entries requise)
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.11.17';
+$plugin->release = 'v1.11.18';
 
